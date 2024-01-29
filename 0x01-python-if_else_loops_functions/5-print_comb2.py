cf9e8bc7bd83c3d2e025838384684dfeb3309370
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-01-29 20:04:33 +0000

    tsted

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+for i in range(0, 100):
+    if (i / 10 < 1):
+        print("0", end="")
+    if i == 99:
+        print("{:d}". format(i))
+    else:
+        print("{:d}".format(i), end=", ")
