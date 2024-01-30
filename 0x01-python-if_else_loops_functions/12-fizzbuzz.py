add1b2f43c1293c819c664244d0ef86ad25a699a
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-01-30 18:05:11 +0000

    ,

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if not i % 15 :
-            print("FizzBuzz", end="" if i == 100 else " ")
+        if not i % 15:
+            print("FizzBuzz", end=" ")
         elif not i % 5:
-            print("Buzz", end="" if i == 100 else " ")
+            print("Buzz", end=" ")
         elif not i % 3:
-            print("Fizz", end="" if i == 100 else " ")
+            print("Fizz", end=" ")
         else:
-             print(i, end="" if i == 100 else " ")
+            print(i, end=" ")
