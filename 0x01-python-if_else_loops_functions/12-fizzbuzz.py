864f09a2bebe61ff125897f652d4a7021735fd90
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-01-30 17:59:12 +0000

    mm

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if not i % 3 :
-            print("Fizz", end="" if i == 100 else " ")
+        if not i % 15 :
+            print("FizzBuzz", end="" if i == 100 else " ")
         elif not i % 5:
             print("Buzz", end="" if i == 100 else " ")
-        elif not i % 15:
-            print("FizzBuzz", end="" if i == 100 else " ")
+        elif not i % 3:
+            print("Fizz", end="" if i == 100 else " ")
         else:
              print(i, end="" if i == 100 else " ")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 for i in range(10):
     for j in range(10):
         if j > i:
-            print("{:d}{:d}".format(i, j), end="\n" if (i == 8 and
-                             j == 9) else ", ")
- 
+            print("{:d}{:d}".format(i, j), end="\n" if (i == 8 andj == 9) 
+                    else ", ")
