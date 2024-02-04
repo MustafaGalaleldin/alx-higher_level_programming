42ff16af0175903ada15321ae61d8896c72deb17
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-02-04 22:05:19 +0000

    ;;

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    l = len(sentence)
+    length = len(sentence)
     ch = sentence[0] if len(sentence) > 0 else None
-    return l, ch
+    return length, ch
