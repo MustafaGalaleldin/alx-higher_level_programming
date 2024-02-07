40a9f48db11a3a0d00e5e11a96f79c1c488330b0
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-02-07 22:01:14 +0000

    correct

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or not a_dictionary:
         return None
     ran = 0
     for k, v in a_dictionary.items():
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     from functools import reduce
+    if not my_list:
+        return 0
     a = list(set(my_list))
     return reduce(lambda x, y: x + y, a)
