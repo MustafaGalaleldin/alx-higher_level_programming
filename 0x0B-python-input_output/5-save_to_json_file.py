d55a0f7af927bcd083b08574fe31eb700bb51667
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-16 23:27:11 +0200

    5th

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+'task 5'
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    'save_to_json_file'
+    with open(filename, mode='w', encoding='UTF8') as f:
+        f.write(json.dumps(my_obj))
