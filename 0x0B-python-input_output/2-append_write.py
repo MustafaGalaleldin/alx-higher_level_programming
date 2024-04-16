43b0c2194ffb9f0957e0de33c62414ed35987a9f
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-16 22:33:32 +0200

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 'task 2'
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     'append in file'
     with open(filename, mode='a') as f:
         return f.write(text)
