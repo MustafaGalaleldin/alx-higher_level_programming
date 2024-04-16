bade8d471fc263c4187aae7aa9c1c1585ef7e9c1
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-17 00:00:44 +0200

    done

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+'6th task'
+
+
+import json
+
+
+def load_from_json_file(filename):
+    'load_from_json_file'
+    with open(filename, mode='r', encoding='UTF8') as f:
+        return json.load(f)
