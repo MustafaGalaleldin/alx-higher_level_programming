d8dc2d10aa7db9b278f5395a1e43598b03940bcc
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-18 04:46:13 +0200

    ll

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" 2nd task """
+'''Module for is_same_class method.'''
 
 
 def is_same_class(obj, a_class):
-    """ def is_same_class """
-    return type(obj) == a_class
+    '''Determines if an object is exactly an instance of a class.'''
+    if type(obj) == a_class:
+        return True
+    return False
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" 4th task """
+'''Module for inherits_from method.'''
 
 
 def inherits_from(obj, a_class):
-    """ def inherits_from """
+    '''Determines if an object is a true subclass of a class.'''
     return isinstance(obj, a_class) and type(obj) != a_class
