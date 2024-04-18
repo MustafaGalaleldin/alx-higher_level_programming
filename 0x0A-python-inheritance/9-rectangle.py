18579ef75abb3e3f601314966a329ed54b1a635a
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-18 04:54:20 +0200

    ko

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-''' 9th task '''
+'''Module for Rectangle class.'''
 BaseGeometry = __import__('7-base_geometery').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ BaseGeometry """
+    '''A subclass representing a rectangle.'''
     def __init__(self, width, height):
-        'init'
+        '''Constructor.'''
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        'area'
+        '''Method which returns area of rectangle.'''
         return self.__width * self.__height
 
     def __str__(self):
-        'str'
+        '''String representation method.'''
         return "[Rectangle]" + str(self.__width) + '/' + str(self.__height)
