00b236768581257a97349e365ee35d2e87d90d83
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-18 03:11:14 +0200

    ll

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" 2nd task """
+
+
+def is_same_class(obj, a_class):
+    """ def is_same_class """
+    if isinstance(obj, a_class):
+        return True
+    return False
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" 3rd task """
+
+
+def is_kind_of_class(obj, a_class):
+    """ def is_kind_of_class """
+    if isinstance(obj, a_class):
+        return True
+    return False
