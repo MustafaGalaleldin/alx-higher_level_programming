f0ffa42f626438b147b4e578cf7539a007f00024
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-18 04:16:09 +0200

    trial

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" 8th task """
+BaseGeometry = __import__('7-base_geometery').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ BaseGeometry """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
