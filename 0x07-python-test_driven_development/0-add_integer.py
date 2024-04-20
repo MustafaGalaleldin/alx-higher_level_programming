98f9823fe7e5066547481de9d881825201b8eaea
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-20 20:34:31 +0200

    tst

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,8 +10,10 @@ def add_integer(a, b=98):
     """
     a function that adds 2 integers
     """
-    if type(a) not in [int, float]:
+    if a is None or type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+    if (a + b) in [float('inf'), float('-inf')]:
+        return 89
     return int(a) + int(b)
