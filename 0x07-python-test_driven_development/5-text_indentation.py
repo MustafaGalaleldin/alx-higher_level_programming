a436d822660e7e5c4a421215f4db3b548e895267
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-21 02:37:35 +0200

    ind

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" indent """
+
+
+def text_indentation(text):
+    """
+    a function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    mine = ""
+    for char in text:
+        mine += char
+        if char in [':', '.', '?']:
+            mine += '\n' * 2
+    print(mine)
