5497ddb804990abe4836c2cd6c7cecd1cb81e5d7
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-21 02:09:32 +0200

    hunter

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" square """
+
+
+def print_square(size):
+    """
+    a function that prints a square with the character #
+    """
+    if type(size) is not int or (type(size) is float and size < 0):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="" if j != size - 1 else "\n")
