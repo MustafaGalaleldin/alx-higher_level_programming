1dc22162380909eb21e56a65a7a303d5fa1b71ba
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-22 07:59:12 +0200

    check

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -10,7 +10,7 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
 
     if type(m_b) is not list:
-        raise TypeError("m_b must be a list ")
+        raise TypeError("m_b must be a list")
 
     m_a_rows, m_a_col, m_b_rows, m_b_col = [0, 0, 0, 0]
 
