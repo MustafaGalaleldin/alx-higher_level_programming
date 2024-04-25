b00a195a085e313315813379b42067ca10fe540a
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-25 03:28:26 +0200

    ?

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,9 @@ from base import Base
 
 
 class Rectangle(Base):
-    """ the class Rectangle that inherits from Base """
+    """
+    the class Rectangle that inherits from Base
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ constructor """
@@ -16,40 +18,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """ width getter"""
+        """ width getter decorator"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ width setter """
+        """ width setter decorator"""
         self.__width = value
 
     @property
     def height(self):
-        """ heigth getter"""
+        """ heigth getter decorator"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ height setter """ 
+        """ height setter decorator""" 
         self.__height = value
 
     @property
     def x(self):
-        """ x getter """
+        """ x getter decorator"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """ x setter """
+        """ x setter decorator"""
         self.__x = value
 
     @property
     def y(self):
-        """ y getter """
+        """ y getter decorator"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """ y setter """
+        """ y setter decorator"""
         self.__y = value
