72967f3d2bac9e9c28d2c5f304483997b78816d4
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-25 03:37:09 +0200

    '''

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,7 +11,7 @@ class Rectangle(Base):
     '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ constructor """
+        ''' constructor '''
         self.__width = width
         self.__height = height
         self.__x = x
