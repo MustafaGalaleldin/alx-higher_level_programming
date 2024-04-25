710fe71f16d894c756fe542925a54bf7e4f7a02b
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-25 17:19:50 +0200

    jj

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,10 +16,10 @@ class Rectangle(Base):
         self.validator('height', height, True)
         self.validator('x', x, False)
         self.validator('y', y, False)
-        '''self.__width = width
+        self.__width = width
         self.__height = height
         self.__x = x
-        self.__y = y'''
+        self.__y = y
         super().__init__(id)
 
     @property
@@ -30,7 +30,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         self.validator('width', value, True)
-        # self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -40,7 +40,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         self.validator('height', value, True)
-        # self.__height = value
+        self.__height = value
 
     @property
     def x(self):
@@ -50,7 +50,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         self.validator('x', value, False)
-        # self.__x = value
+        self.__x = value
 
     @property
     def y(self):
@@ -60,7 +60,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         self.validator('y', value, False)
-        # self.__y = value
+        self.__y = value
 
     def validator(self, name, val, sizes=True):
         ''' validating method '''
@@ -70,5 +70,7 @@ class Rectangle(Base):
             raise ValueError(f"{name} must be > 0")
         elif not sizes and val < 0:
             raise ValueError(f"{name} must be >= 0")
-        else:
-            self.name = val
+
+    def area(self):
+        ''' return recangle area '''
+        return self.width * self.height
