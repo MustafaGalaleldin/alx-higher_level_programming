4864a18a43451c3f850f966032b720282a5017a6
mustafagalaleldin@gmail.com <mustafagalaleldin@gmail.com>
2024-05-23 18:48:17 +0300

    .

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 --  creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT UNIQUE NOT NULL,
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
+    PRIMARY KEY (id));
