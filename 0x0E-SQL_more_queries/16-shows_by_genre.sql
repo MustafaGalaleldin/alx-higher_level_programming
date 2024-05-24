fef32178c872eeec95bdea44487620e94592504e
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-05-24 19:59:44 +0300

    commented

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
---  lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 SELECT title, name 
 FROM tv_genres 
 JOIN tv_show_genres 
