9aee528a9b34ffb54ae142794c25cb792541d4b2
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-05-24 19:21:10 +0300

    fixed

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,4 +5,4 @@ FROM tv_genres
 JOIN tv_show_genres 
 ON tv_genres.id = tv_show_genres.genre_id 
 GROUP BY tv_show_genres.genre_id 
-ORDER BY 'Number of shows linked to this genre' DESC, genre ASC;
+ORDER BY `Number of shows linked to this genre` DESC, genre ASC;
