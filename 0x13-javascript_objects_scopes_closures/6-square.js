c3bb84124fdb4157abde5d34735d31735ceeaa49
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-06-20 05:49:54 +0300

    DONE

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const PSquare = require('./5-square');
+class Square extends PSquare {
+  charPrint (c) {
+    let shape;
+    if (!c) {
+      shape = 'X';
+    } else {
+      shape = c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(shape.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
