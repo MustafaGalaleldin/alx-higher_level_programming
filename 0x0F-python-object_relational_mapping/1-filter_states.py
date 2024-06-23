c44cff5091fcac27b2fc049b3da1a3b965db190a
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-06-23 23:20:45 +0300

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+    "check if executed"
+    import MySQLdb
+    import sys
+
+    argv = sys.argv
+    if len(argv) == 4:
+        name = argv[1]
+        password = argv[2]
+        db = argv[3]
+        conn = MySQLdb.connect(host='localhost', port=3306, user=name,
+                                    passwd=password, db=db, charset='utf8')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE\
+                    REGEXP_LIKE(states.name, '^N.+') ORDER BY states.id")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        conn.close()
