8f68c150fca335a469c097d358c567a404ad1370
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-06-24 01:03:40 +0300

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""takes in an argument and displays all values in the states table of
+    hbtn_0e_0_usa where name matches the argument"""
+
+if __name__ == '__main__':
+    "check if executed"
+    import MySQLdb
+    import sys
+
+    argv = sys.argv
+    if len(argv) == 5:
+        name = argv[1]
+        password = argv[2]
+        db = argv[3]
+        search = argv[4]
+        conn = MySQLdb.connect(host='localhost', port=3306, user=name,
+                                    passwd=password, db=db, charset='utf8')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE\
+                    states.name LIKE '{}' ORDER BY states.id".format(search))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        conn.close()
