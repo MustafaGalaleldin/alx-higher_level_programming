ec0c71a4d7d60b6cf650099cda5302675a871f52
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-06-24 03:33:49 +0300

    tst

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""takes in arguments and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument. But this time,
+write one that is safe from MySQL injections!"""
+
+if __name__ == '__main__':
+    "check if executed"
+    import MySQLdb
+    import sys
+
+    argv = sys.argv
+    if len(argv) == 5:
+        name = argv[1]
+        password = argv[2]
+        db = argv[3]
+        search = argv[4]
+        conn = MySQLdb.connect(host='localhost', port=3306, user=name,
+                                    passwd=password, db=db, charset='utf8')
+        cur = conn.cursor()
+        query = "SELECT * FROM states WHERE name = %s"
+        uinput = search
+        cur.execute(query, (uinput,))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        conn.close()
