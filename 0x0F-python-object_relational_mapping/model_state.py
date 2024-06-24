64f3ce7ddd6275d434c15fd5e01d4a3133523120
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-06-25 01:09:51 +0300

    irst state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""a python file that contains the class definition of a State
+and an instance Base = declarative_base()"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    "class to define state objects and manipulate their table"
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
