75611ef156b9908b847abe9858a69ac86fcfd690
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-06-27 20:57:37 +0300

    n

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 "a script that lists all State objects from the database hbtn_0e_6_usa"
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+if __name__ == '__main__':
+    "do not run if imported"
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
 
-argv = sys.argv
-if len(argv) == 4:
-    name = argv[1]
-    password = argv[2]
-    db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(name, password, db), pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for n, a_state in enumerate(session.query(State).all(), start=1):
-        print("{}: {}".format(n, a_state.name))
-    session.close()
+    argv = sys.argv
+    if len(argv) == 4:
+        name = argv[1]
+        password = argv[2]
+        db = argv[3]
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(name, password, db), pool_pre_ping=True)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        for a_state in session.query(State).all():
+            print("{}: {}".format(a_state.id, a_state.name))
+        session.close()
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"prints the first State object from the database hbtn_0e_6_usa"
+
+if __name__ == '__main__':
+    "do not run if imported"
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    import sys
+    from model_state import Base, State
+
+    argv = sys.argv
+    if len(argv) == 4:
+        name = argv[1]
+        password = argv[2]
+        db_name = argv[3]
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(name, password, db_name))
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        state = session.query(State).first()
+        if state:
+            print(f"{state.id}: {state.name}")
+        else:
+            print('Nothing')
+        session.close()
