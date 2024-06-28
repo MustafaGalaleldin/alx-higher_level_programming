ad9216545de6d687cfb718bf16a6a4e4a2e058b4
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-06-28 21:33:13 +0300

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"prints all City objects from the database hbtn_0e_14_usa"
+
+from sqlalchemy import create_engine
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+from model_city import City
+
+argv = sys.argv
+if len(argv) == 4:
+    user = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(user, password, db_name))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+    cities = session.query(City).order_by(City.id).all()
+    states = session.query(State).all()
+    for city in cities:
+        state = session.query(State).filter_by(id=city.state_id).first()
+        print(f"{state.name}: ({city.id}) {city.name}")
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"contains the class definition of a City"
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    "class to define cities objects and manipulate their table"
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
