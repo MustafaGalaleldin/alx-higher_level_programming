4c0e1efee5d26b786d35b5513f06d14234543a74
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-07-05 05:37:47 +0300

    j

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept
-curl -sLI "$1" | grep -i "Allow: " | cut -d ' ' -f2-
+curl -sLI "$1" | grep -i "Allow: " | cut -d ' ' -f 2
