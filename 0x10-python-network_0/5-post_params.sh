0abd5bbac295e79b910c680832fc84b3bb24af14
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-07-05 05:33:06 +0300

    1

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+#  takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s-H "POST" --variable email=test@gmail.com --variable subject='I will always be here for PLD' "$1"
