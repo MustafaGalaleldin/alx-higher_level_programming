c3533ccea4a2204076451439fef39764ac0aa8de
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-07-06 01:52:42 +0300

    pip8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,10 +6,10 @@ https://alx-intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == '__main__':
-	url = 'https://alx-intranet.hbtn.io/status'
-	with urllib.request.urlopen(url) as response:
-		page = response.read()
-		print('Body response:')
-		print('\t- type:', type(page))
-		print('\t- content:', page)
-		print('\t- utf8 content:', page.decode('utf-8'))
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        page = response.read()
+        print('Body response:')
+        print('\t- type:', type(page))
+        print('\t- content:', page)
+        print('\t- utf8 content:', page.decode('utf-8'))
