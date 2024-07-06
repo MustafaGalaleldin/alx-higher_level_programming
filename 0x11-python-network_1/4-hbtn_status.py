26bd21bbcbd488be64980e5b6a2d339f6ce863b8
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-07-06 04:01:59 +0300

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''
+a Python script that fetches https://alx-intranet.hbtn.io/status
+'''
+import requests
+
+if __name__ == '__main__':
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    page = response.text
+    print('Body response:')
+    print('\t- type:', type(page))
+    print('\t- content:', page)
