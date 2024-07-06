cf8efa38f9a0aa305c38110151c5d02011db213b
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-07-06 05:08:37 +0300

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''
+ a Python script that takes in a URL, sends a request
+ to the URL and displays the body of the response.
+'''
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
+    except requests.exceptions.RequestException as e:
+        print('Error code:', response.status_code)
